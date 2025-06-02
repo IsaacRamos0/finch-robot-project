f5edcb4632be6989d3621fef427b05a7e1949662
IsaacRamos0 <iramos0054@mymail.lausd.net>
2025-06-02 10:40:39 -0700

    updated from today

--- src/main/java/com/isquared/Test.java
+++ src/main/java/com/isquared/Test.java
@@ -1,3 +1,27 @@
-public class Test {
+package com.isquared;
+
+import com.birdbrain.Finch;
+
+public class FinchMoveTurn {
+    public static void main(String[] args) {
+        Finch bird = new Finch();
+
+bird.setMove("F", 35, 100);
+bird.setMove("B", 35, 100);
+bird.setMove("F", 35, 100);
+bird.setMove("B", 35, 100);
+bird.setTurn("R", 90, 50);
+bird.setMove("F", 35, 100);
+bird.setMove("B", 35, 100);
+bird.setMove("F", 35, 100);
+bird.setMove("B", 35, 100); 
+
+bird.setBeak(100, 0, 0);
+bird.pause(3);
+bird.setBeak(0, 0, 0);
+
+
     
+        bird.disconnect();
+    }
 }
